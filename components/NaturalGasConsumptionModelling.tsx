57ca9bac16bb4a4ae80f8666029917d10a45ec95
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-21 17:11:55 +0200

    Added chart

--- components/NaturalGasConsumptionModelling.tsx
+++ components/NaturalGasConsumptionModelling.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Chart from "chart.js/auto";
 
 export const NaturalGasConsumptionModelling = () => {
   // Get the data from the Google Cloud Storage backend
@@ -17,5 +18,75 @@ export const NaturalGasConsumptionModelling = () => {
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
-  return <p>{JSON.stringify(data)}</p>;
+
+  // Draw chart once data is loaded
+  const chartRef = React.useRef<any>(null);
+  React.useEffect(() => {
+    if (Object.keys(data).length === 0) return;
+
+    const historicConsumptionData = Object.values(
+      data.historic_consumption_data
+    );
+    const historicTemperatureData = Object.values(
+      data.historic_temperature_data
+    );
+
+    const forecastConsumptionData = Object.values(
+      data.natural_gas_consumption_forecast
+    );
+
+    const timeline = [
+      ...Object.keys(data.historic_consumption_data),
+      ...Object.keys(data.natural_gas_consumption_forecast),
+    ];
+
+    const ctx = document.getElementById("chart") as HTMLCanvasElement;
+
+    // Make sure canvas is not in use
+    // if (ctx) ctx.remove();
+
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: timeline,
+        datasets: [
+          {
+            label: "Historic Natural Gas Consumption",
+            data: historicConsumptionData,
+            fill: false,
+            borderColor: "rgba(230, 230, 230, 0.6)",
+            tension: 0.1,
+          },
+          {
+            label: "Historic Temperatures",
+            data: historicTemperatureData,
+            fill: false,
+            borderColor: "rgba(255, 99, 132, 1.0)",
+            tension: 0.1,
+          },
+          {
+            label: "Natural Gas Consumption Forecast",
+            data: [
+              ...Array(historicConsumptionData.length).fill(null),
+              ...forecastConsumptionData,
+            ],
+            fill: false,
+            borderColor: "rgba(230, 230, 230, 1.0)",
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+      },
+    });
+
+    return () => {
+      myChart.destroy()
+    }
+  }, [data]);
+
+  // Make canvas full width but keep aspect ratio
+  return <canvas id="chart" ref={chartRef} className="w-full" />;
 };
